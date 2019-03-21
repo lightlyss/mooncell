0116e66d5c2190fbe902d4b2bd502c7be9c8c2e9
Chara <37601555+lightlyss@users.noreply.github.com>
2019-03-20 21:15:38 -0400

    resolve #8 and implement indexing engine (#9)

--- js/seraph.js
+++ js/seraph.js
@@ -1,5 +1,6 @@
 const low = require('lowdb');
 const FileSync = require('lowdb/adapters/FileSync');
+const Engine = require('minisearch');
 
 /**
 * Serial Phantasm: SE.RA.PH
@@ -8,21 +9,33 @@ class Seraph {
   constructor(path) {
     this.adapter = new FileSync(path);
     this.db = low(this.adapter);
+    this.engine = new Engine({
+      fields: ['name', 'className'],
+      searchOptions: {
+        prefix: true,
+        fuzzy: 0.1,
+        combineWith: 'AND'
+      }
+    });
+    this.engine.addAll(this.db.get('servants').value());
   }
 
   query(terms) {
+    let results = this.engine.search(terms.join(' '));
+    if (!results || !results.length) return null;
+    let ids = [];
+    for (let i = 0; i < results.length; i++) {
+      if (results[i].score != results[0].score) break;
+      ids.push(results[i].id);
+    }
+    ids.sort((a, b) => parseFloat(a) - parseFloat(b));
+    return this.findById(ids[0]);
+  }
+
+  findById(id) {
     return this.db
       .get('servants')
-      .find(svt => {
-        let normName = svt.name.toLowerCase();
-        let normClass = svt.className.toLowerCase();
-        for (let term of terms) {
-          let t = term.toString().toLowerCase();
-          if (normName.includes(t) || normClass.includes(t)) continue;
-          return false;
-        }
-        return true;
-      })
+      .find({id: id})
       .value();
   }
 
