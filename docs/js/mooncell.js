2dc27e6194eb6f93cb10282f58c257c5b31b1159
Chara <37601555+lightlyss@users.noreply.github.com>
2019-05-04 00:44:10 -0400

    resolve #17

--- docs/js/mooncell.js
+++ docs/js/mooncell.js
@@ -17,17 +17,44 @@ app.service('automaton', class {
   }
 });
 
+app.directive('mcSkill', $timeout => {
+  return {
+    restrict: 'A',
+    link: (scope, element) => $timeout(() => {
+      $(element).popover();
+    }, 0)
+  };
+});
+
 app.controller('mooncellCtrl', async ($scope, $window, automaton) => {
   const seraph = new $window.Seraph(await automaton.getAdapter());
   const sheba = new $window.Sheba('img/');
+  const escape = s => $('<div>').text(s).html();
+  const appendSummaryHTML = o => {
+    o.summary = o.effects.map(escape).join('<br>');
+  };
+
   $scope.search = query => {
     if (!query) return;
     const svt = seraph.query(query.trim().split(/ +/));
     if (!svt || !svt.id) return;
     $scope.imgPath = sheba.getImgPath(svt.id);
     $scope.svt = svt;
+
+    $scope.actives = svt.actives
+      .map(seraph.findActiveById.bind(seraph))
+      .filter(a => a);
+    $scope.actives.forEach(appendSummaryHTML);
+
+    $scope.passives = svt.passives
+      .map(seraph.findPassiveById.bind(seraph))
+      .filter(p => p);
+    $scope.passives.forEach(appendSummaryHTML);
+
+    $scope.np = seraph.findNoblePhantasmById(svt.id);
+    appendSummaryHTML($scope.np);
   };
 
   $scope.search('BB');
-  return $scope.$digest();
+  return $scope.$apply();
 });
